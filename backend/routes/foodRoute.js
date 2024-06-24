c52cd1429a04038010e5f5cb020f74be1d9febc4
Akshay Chaudhari <chaudhariap2003@gmail.com>
2024-06-24 19:26:36 +0530

    initial commit

---
+++ backend/routes/foodRoute.js
@@ -0,0 +1,38 @@
+import express from "express";
+import {
+  addFood,
+  listFood,
+  removeFood,
+} from "../controllers/foodController.js";
+import multer from "multer";
+import path from "path";
+import mkdirp from "mkdirp";
+import { fileURLToPath } from "url";
+
+// Create __dirname equivalent for ES modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const foodRouter = express.Router();
+
+// Ensure the upload directory exists
+const uploadDir = path.join(__dirname, "../uploads");
+mkdirp.sync(uploadDir); // Create the uploads directory if it doesn't exist
+
+// Image Storage Engine (Saving Image to /uploads folder & rename it)
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, uploadDir);
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}${file.originalname}`);
+  },
+});
+
+const upload = multer({ storage: storage });
+
+foodRouter.get("/list", listFood);
+foodRouter.post("/add", upload.single("image"), addFood);
+foodRouter.post("/remove", removeFood);
+
+export default foodRouter;
---
+++ backend/server.js
@@ -0,0 +1,54 @@
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./config/db.js";
+import userRouter from "./routes/userRoute.js";
+import foodRouter from "./routes/foodRoute.js";
+import "dotenv/config";
+import cartRouter from "./routes/cartRoute.js";
+import orderRouter from "./routes/orderRoute.js";
+
+// app config
+const app = express();
+const port = process.env.PORT || 4000;
+
+// middlewares
+app.use(express.json());
+
+const allowedOrigins = [
+  // "https://food-delivery-website-gamma.vercel.app",
+  // "https://food-delivery-website-admin.vercel.app"
+  "http://localhost:5173",
+  "http://localhost:5174",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["POST", "GET", "PUT", "DELETE"],
+    credentials: true,
+  })
+);
+
+// db connection
+connectDB();
+
+// api endpoints
+app.use("/api/user", userRouter);
+app.use("/api/food", foodRouter);
+app.use("/images", express.static("uploads"));
+app.use("/api/cart", cartRouter);
+app.use("/api/order", orderRouter);
+
+app.get("/", (req, res) => {
+  res.send("API Working");
+});
+
+app.listen(port, () =>
+  console.log(`Server started on http://localhost:${port}`)
+);
