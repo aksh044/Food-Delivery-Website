54fb80e6c765717f2e6517041ed82a934bccf5ef
Akshay Chaudhari <chaudhariap2003@gmail.com>
2024-06-24 22:19:08 +0530

    cors updAate

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const allowedOrigins = [
   "https://food-delivery-website-admin-msgk.onrender.com",
   "http://localhost:5173",
   "http://localhost:5174",
-  "http://nyahari.tech",
+  "http://www.nyahari.tech",
 ];
 
 app.use(
